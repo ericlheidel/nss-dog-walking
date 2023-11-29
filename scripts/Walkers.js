f416b79bda184bc657108f4b0799d226cad9c731
Eric Heidel <ericleeheidel@yahoo.com>
2023-11-29 12:58:18 -0600

    Completed Project

---
+++ scripts/Walkers.js
@@ -0,0 +1,34 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (theClickevent) => {
+        const whatWasClickedOn = theClickevent.target
+
+        if (whatWasClickedOn.dataset.type === "walker") {
+            window.alert(`This walker works in ${whatWasClickedOn.dataset.city}`)
+        }
+
+    }
+)
+
+export const Walkers = () => {
+    let walkerHTML = "<ul>"
+
+    for (const walker of walkers) {
+        walkerHTML += `
+                    <li
+                        data-id="${walker.id}"
+                        data-city="${walker.city}"
+                        data-type="walker"
+                    >${walker.name}
+                    </li>`
+    }
+
+    walkerHTML += "</ul>"
+
+    return walkerHTML
+}
+
